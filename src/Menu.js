f79b02c45887eada3543ee69b9afa8d097f8008a
mrprofessor <kar.rudra008@gmail.com>
2017-10-03 20:12:48 +0530

    create-react-app

---
+++ src/Menu.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './Menu.css'
+
+class Menu extends Component {
+
+    render() {
+        return (
+            <ul className="menuBar">
+                <li><a>Books</a></li>
+                <li><a>Add</a></li>
+                <li><a>asdfasd</a></li>
+            </ul>
+            
+        );
+    }
+}
+
+export default Menu;
